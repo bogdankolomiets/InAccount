5eb8dd2c1f59a0237e1c925784167a06c8e23cd1
bkolomiets <kolom996@gmail.com>
2017-03-06 12:31:38 +0200

    add custom App class

---
+++ app/src/main/java/com/bogdankolomiets/inaccount/App.java
@@ -0,0 +1,12 @@
+package com.bogdankolomiets.inaccount;
+
+import android.app.Application;
+
+/**
+ * @author bogdan
+ * @version 1
+ * @date 06.03.17
+ */
+
+public class App extends Application {
+}
