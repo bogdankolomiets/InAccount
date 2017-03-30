b22ff8590cfb6890e99765d05b27938e01d93287
bkolomiets <kolom996@gmail.com>
2017-03-30 20:09:17 +0300

    add dagger 2 and rxjava 2. Develop ApiModule

---
+++ app/src/main/java/com/bogdankolomiets/inaccount/ui/MainActivity.java
@@ -0,0 +1,20 @@
+package com.bogdankolomiets.inaccount.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.bogdankolomiets.inaccount.ui.common.BaseActivity;
+
+/**
+ * @author Bogdan Kolomiets
+ * @version 1
+ * @date 30.03.17
+ */
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/bogdankolomiets/inaccount/ui/common/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.bogdankolomiets.inaccount.ui.common;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Bogdan Kolomiets
+ * @version 1
+ * @date 30.03.17
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
